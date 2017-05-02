fd8266276662a29a1794a85cdac1409fd8a5dcc2
Andrew Silva <asilva@law.harvard.edu>
2017-05-02 16:59:38 -0400

    Scrum reminder between 11:00AM and 11:04AM on the ScreenShare screens.

--- main.js
+++ main.js
@@ -5,21 +5,32 @@ var bucket_url = 'https://s3.amazonaws.com/lilscreenshare/';
 
 setInterval(check_for_new_image, 1000);
 
-function check_for_new_image() {		
-	$.ajax({
-		dataType: "json",
-		url: bucket_url + 'current.json',
-		cache: false,
-		success: function(data) {
-				if (data.name !== current_filename) {
-					current_filename = data.name;
-					$('#image-container').attr("src", bucket_url + current_filename);
-				}
+function check_for_new_image() {	
+	var todayHours = new Date().getHours();
+	var todayMinutes = new Date().getMinutes();
 
-				if (data['background-color'] !== current_bg_color) {
-					current_bg_color = data['background-color']
-					$('body').css('background-color', current_bg_color);
-				}
-		}
-	});
+	// Between 11:00AM and 11:05AM display big red "SCRUM" image instead of regular image
+	if (todayHours === 11 && todayMinutes >= 00 && todayMinutes < 05) {
+		current_filename = "./SCRUM.png";
+		$('#image-container').attr("src", current_filename);
+		current_bg_color = "RED"
+		$('body').css('background-color', current_bg_color);
+	} else {
+		$.ajax({
+			dataType: "json",
+			url: bucket_url + 'current.json',
+			cache: false,
+			success: function(data) {
+					if (data.name !== current_filename) {
+						current_filename = data.name;
+						$('#image-container').attr("src", bucket_url + current_filename);
+					}
+
+					if (data['background-color'] !== current_bg_color) {
+						current_bg_color = data['background-color']
+						$('body').css('background-color', current_bg_color);
+					}
+			}
+		});
+	}
 }
