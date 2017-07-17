a76fe92b02c0b39f2e8ba95a1c0831c6972f9a59
Matt Phillips <mphillips@law.harvard.edu>
2017-07-17 15:58:17 -0400

    Some cleanup of the multi-screen logic

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var current_filename = '';
 var current_bg_color = '#fff';
 
-var bucket_url = 'https://s3.amazonaws.com/lilscreenshare/';
+var bucket_url = 'https://s3.amazonaws.com/lilscreensharetest/';
 
 setInterval(check_for_new_image, 1000);
 
-function check_for_new_image() {	
+function check_for_new_image() {
 	var todayHours = new Date().getHours();
 	var todayMinutes = new Date().getMinutes();
 
@@ -21,15 +21,27 @@ function check_for_new_image() {
 			url: bucket_url + 'current.json',
 			cache: false,
 			success: function(data) {
-					if (data.name !== current_filename) {
-						current_filename = data.name;
-						$('#image-container').attr("src", bucket_url + current_filename);
+				if(window.location.hash === '#racehorse') {
+					if (data.hasOwnProperty("racehorse") && data['racehorse'].url !== current_filename) {
+						current_filename = bucket_url + data['racehorse'].url;
+						$('#image-container').attr("src", current_filename);
 					}
 
-					if (data['background-color'] !== current_bg_color) {
-						current_bg_color = data['background-color']
-						$('body').css('background-color', current_bg_color);
+				}
+
+				if(window.location.hash === '#banana') {
+					if (data.hasOwnProperty("banana") && data['banana'].url !== current_filename) {
+						current_filename = bucket_url + data['banana'].url;
+						$('#image-container').attr("src", current_filename);
 					}
+				}
+
+				if(window.location.hash === '#icecream') {
+					if (data.hasOwnProperty("icecream") && data['icecream'].url !== current_filename) {
+						current_filename = bucket_url + data['icecream'].url;
+						$('#image-container').attr("src", current_filename);
+					}
+				}
 			}
 		});
 	}
